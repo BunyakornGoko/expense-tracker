7d18610b9e22bd66b266e534cd327ded0ccd7056
BunyakornGoko <bunyakorn.porn@gmail.com>
2026-09-06 21:51:07 +0700

    Delete kuromi mask

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,8 @@
 import { LoginForm } from '@/components/login-form'
-import { KuromiMark } from '@/components/kuromi-mark'
 
 export default function LoginPage() {
   return (
     <main className="auth-shell">
-      <KuromiMark />
       <LoginForm />
     </main>
   )
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,8 @@
 import { RegisterForm } from '@/components/register-form'
-import { KuromiMark } from '@/components/kuromi-mark'
 
 export default function RegisterPage() {
   return (
     <main className="auth-shell">
-      <KuromiMark />
       <RegisterForm />
     </main>
   )
