1a2872786223d9eb8266355c70e76a0b2a4f8204
BunyakornGoko <bunyakorn.porn@gmail.com>
2026-09-07 23:36:10 +0700

    Change icon shortcut

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Kuromi Money Diary',
+    name: 'Kuromi Diary',
     short_name: 'Kuromi Diary',
     description: 'จัดการรายรับรายจ่ายของคุณในสไตล์ Kuromi',
     start_url: '/',
@@ -10,8 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#f4f1f8',
     theme_color: '#6c46ad',
     icons: [
-      { src: '/Kuromi.png', sizes: '240x240', type: 'image/png' },
-      { src: '/Kuromi%20List.webp', sizes: '360x360', type: 'image/webp' },
+      { src: '/Kuromi.png', sizes: '240x240', type: 'image/png' }
     ],
   }
 }
