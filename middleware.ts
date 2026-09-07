65c13c891f799393dc61b20cb99474f828b3f8c8
BunyakornGoko <bunyakorn.porn@gmail.com>
2026-09-07 23:01:25 +0700

    Add bg in login register page

--- middleware.ts
+++ middleware.ts
@@ -22,5 +22,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|webp|gif|svg|ico)$).*)'],
 }
